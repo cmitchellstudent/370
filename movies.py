a611da07540ec51631e84a3d2d641ea1477a4084
cmitchellstudent <88793270+cmitchellstudent@users.noreply.github.com>
2024-03-11 20:02:56 -0500

    added more

--- movies.py
+++ movies.py
@@ -7,10 +7,10 @@ Test account:
 '''
 
 
-
-import requests
 import sys
 import re
+
+import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
 import json
@@ -23,20 +23,50 @@ from time import sleep, time
 # Don't bother constructing console objects in every function.
 
 import argparse
+
+from nltk.corpus.reader import reviews
+
 parser = argparse.ArgumentParser(description='letterboxd args')
 parser.add_argument('--user', '-u', dest='user', help='letterboxd.com user')
 parser.add_argument('--reviews', '-r', dest='reviews', action="store_true", default=False, help='Gets reviews')
-parser.add_argument('--testing', '-t', dest='testing', action='store_true', default=False, help='Testing flag - for development only')
-parser.add_argument('--save-json', '-j', dest='json', action="store_true", default=False, help='Saves a JSON file of the reviews dictionary')
-parser.add_argument('--save-html', '-w', dest='html', action="store_true", default=False, help='Saves an HTML document for easily viewing reviews')
-parser.add_argument('--browser-open', '-b', dest='browserOpen', action="store_true", default=False, help='Opens saved HTML document in the browser')
+parser.add_argument('--testing', '-t', dest='testing', action='store_true', default=False,
+                    help='Testing flag - for development only')
+parser.add_argument('--save-json', '-j', dest='json', action="store_true", default=False,
+                    help='Saves a JSON file of the reviews dictionary')
+parser.add_argument('--save-html', '-w', dest='html', action="store_true", default=False,
+                    help='Saves an HTML document for easily viewing reviews')
+parser.add_argument('--browser-open', '-b', dest='browserOpen', action="store_true", default=False,
+                    help='Opens saved HTML document in the browser')
 parser.add_argument('--search', '-s', nargs='+', dest='search', default=())
 args = parser.parse_args()
 
-
 from rich.console import Console
 from rich import print as rprint
+
 console = Console()
+args.user = "sapphicquinn"
+args.reviews = False
+args.html = False
+args.json = False
+
+
+#How many reviews do i have?
+"""
+import os
+directory = "C:\\Users\Connor\Desktop\letterboxd\letterboxdCorpus"
+total = 0
+for file in os.listdir(directory):
+    if file.endswith('.json'):
+        file_path = os.path.join(directory, file)
+        with open(file_path, 'r') as file1:
+            data = file1.readlines()
+            # Counting lines in JSON file
+            line_count = len(data)
+            total += line_count
+            total -= 5
+rprint("total reviews: ", total)
+"""
+
 # =======================================================================================================================================================================
 
 # Make a list of the pages of reviews. (Ex. /user/films/reviews/page/1 ... /user/films/reviews/page/5)
@@ -60,24 +90,32 @@ def getReviewUrls(user):
 
 
 def getSingleReview(url=''):
-    for possibleUrl in (url, url + '/1/'): # super-8 review had a an extra /1/ on the end
+    for possibleUrl in (url, url + '/1/'):  # super-8 review had a an extra /1/ on the end
         soup = BeautifulSoup(requests.get(possibleUrl).text, 'html.parser')
         reviewDivHtmlStr = str(soup.find("div", {'class': "review body-text -prose -hero -loose"}))
         sleep(.05)
-        if not reviewDivHtmlStr  == 'None':
-            return '<p>' + reviewDivHtmlStr.split('<div><p>')[-1].replace('</div>', '')
-
+        if not reviewDivHtmlStr == 'None':
+            my_string = '<p>' + reviewDivHtmlStr.split('<div><p>')[-1].replace('</div>', '')
+            my_string = re.sub(r'<(\/?p|br).*?>', ' ', my_string)
+            return clean(my_string)
 
 
 # Should probably make different functions for batch getting all movies and searching.
 # Because args.search should only be checked once, not every time there's another movie review.
 # But it was too easy to just throw that in and add a continue
-
+def clean(string):
+    string = string.replace('\n', ' ').replace('\\', ' ').replace('’', '\'')
+    string = string.replace('“', '\"').replace('”', '\"').replace("…", "...")
+    string = string.replace('This review may contain spoilers. I can handle the truth.', ' ')
+    return string
 def getReviews(user):
     movieDelim = f'[red]{"=" * 80}'
     look = f'/{user}/film/'
     reviewsText = {}
 
+
+    #not using search
+    """
     if args.search:
         console.print(movieDelim)
         for url in [f'https://letterboxd.com/{user}/film/{movie}/' for movie in args.search]:
@@ -89,9 +127,7 @@ def getReviews(user):
 
             reviewsText[movie] = getSingleReview(url=url)
         return reviewsText
-
-
-
+    """
     reviewUrls = getReviewUrls(user)
     console.print('[cyan] Urls with multiple reviews')
     rprint(reviewUrls)
@@ -110,24 +146,66 @@ def getReviews(user):
         valuableEnd = htmlText.find('</section>', valuableStart)
         smallStr = htmlText[valuableStart:valuableEnd]
 
-        for line in [line for line in smallStr.splitlines() if line.strip()[10:24] =='"film-detail">']:
+        #MOST OF MY CODE===============================================================================================
 
+        titles = []
+        reviews = []
+        for line in smallStr.splitlines():
+            soup = BeautifulSoup(line, 'html.parser')
+            div_tags = soup.find_all('div')
+            for div_tag in div_tags:
+                title = div_tag.get('data-film-slug')
+                if title is not None:
+                    titles.append(title)
+
+            ratingIndex = line.find('-green rated-')
+            chopped0 = line[ratingIndex:]
+            rating = ""
+            if ratingIndex != -1:
+                rating = chopped0[13:15] + "/10"
+                rating = rating.replace("\"", "")
+            reviewStart = line.find('-prose')
+            if reviewStart != -1:
+                chopped = line[reviewStart:]
+                soup = BeautifulSoup(chopped, 'html.parser')
+                paragraphs = [p.get_text() for p in soup.find_all('p')]
+                paragraphs = [s + " " for s in paragraphs]
+                #rprint(paragraphs)
+                reviewStr = "\n".join(paragraphs)
+                reviewStr = rating + " " + reviewStr
+                reviews.append(clean(reviewStr))
+
+        for i in range (len(titles)):
+            #if review contains weird ...
+            if "…" in reviews[i]:
+                reviewsText[titles[i]] = getSingleReview(url=f'https://letterboxd.com/{user}/film/{titles[i]}/')
+            #if review fits in the preview
+            else:
+                reviewsText[titles[i]] = reviews[i]
+
+
+
+        """
+        #doesnt run this loop for some reason
+        for line in [line for line in smallStr.splitlines() if line.strip()[10:24] == '"film-detail">']:
 
             shorterLine = line[line.find('<p>'):]
 
-            movieSearch = re.search( fr'{look}.*?/', shorterLine).group()
-            movie = re.sub(fr'{look}','', str(movieSearch))[:-1]
+            movieSearch = re.search(fr'{look}.*?/', shorterLine).group()
+            movie = re.sub(fr'{look}', '', str(movieSearch))[:-1]
+
 
             console.print(f'[cyan]movie: [bold blue]{movie}')
             reviewPreview = shorterLine[:shorterLine.find('</div>')].strip()
 
-            if '…' == reviewPreview[-5]: #NOT THREE PERIODS - DIFFERENT UNICODE CHAR
+            if '…' == reviewPreview[-5]:  # NOT THREE PERIODS - DIFFERENT UNICODE CHAR
                 movieReviewUrl = f'https://letterboxd.com/{user}/film/{movie}/'
                 console.print('\t[magenta]Preview contains partial review')
                 console.print(f'\t[magenta]Requesting: {movieReviewUrl}')
                 console.print(movieDelim)
 
                 reviewsText[movie] = getSingleReview(url=movieReviewUrl)
+                console.print(reviewsText)
 
             else:
                 console.print('\t[blue]Preview contains full review')
@@ -135,11 +213,11 @@ def getReviews(user):
                 console.print(movieDelim)
 
                 reviewsText[movie] = reviewPreview
-
-
+                console.print(reviewsText)
 
             sleep(.05)
-
+            """
+    #rprint(reviewsText)
     return reviewsText
 
 
@@ -152,7 +230,7 @@ def writeReviews(reviewsDict={}):
         fname = f'{user}_searched_reviews.html'
     rprint(f'html={fname}')
 
-    with open(fname, 'w+') as f:
+    with open(fname, 'w+', encoding="utf-16") as f:
         f.write('<!DOCTYPE html>\n')
         f.write('<html>\n')
         f.write('<head>\n')
@@ -181,8 +259,9 @@ def letterboxdRun():
     if args.reviews:
         fname = f'{user}_reviews.json'
         reviewsText = getReviews(user)
-
+        console.print(reviewsText)
         outputDict = {'user': user, 'reviews': reviewsText}
+
 
         if args.html:
             writeReviews(outputDict)
@@ -196,9 +275,10 @@ def letterboxdRun():
 
 if __name__ == '__main__':
     console.print('[cyan]*Command line arguments* ')
-    for k,v in vars(args).items():
+    for k, v in vars(args).items():
         rprint(f'\t{k}={v}')
     print()
 
-    console.print('[cyan]--Making requests to letterboxd.com--\n[red]This may take some time depending on how many reviews there are.\n')
+    console.print(
+        '[cyan]--Making requests to letterboxd.com--\n[red]This may take some time depending on how many reviews there are.\n')
     letterboxdRun()
---
+++ test.py
@@ -0,0 +1,36 @@
+import re
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from time import sleep, time
+import os
+import json
+"""
+def getReviewPage(url=''):
+    for possibleUrl in (url, url + '/1/'):  # super-8 review had a an extra /1/ on the end
+        soup = BeautifulSoup(requests.get(possibleUrl).text, 'html.parser')
+        reviewDivHtmlStr = str(soup.find("div", {'class': "review body-text -prose -hero -loose"}))
+        sleep(.05)
+        if not reviewDivHtmlStr == 'None':
+            my_string = '<p>' + reviewDivHtmlStr.split('<div><p>')[-1].replace('</div>', '')
+            print(my_string)
+            return re.sub(r'<(\/?p|br).*?>', ' ', my_string)
+            
+
+print(getReviewPage("https://letterboxd.com/redlettermedia/film/drive-angry/"))
+"""
+
+directory = "C:\\Users\Connor\Desktop\letterboxd\letterboxdCorpus"
+
+dfs = []
+for filename in os.listdir(directory):
+        if filename.endswith('.json'):
+            file_path = os.path.join(directory, filename)
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+                userDf = pd.DataFrame.from_dict(data)
+                dfs.append(userDf)
+df = pd.concat(dfs, ignore_index=False)
+print(df)
+#df.to_csv("C:/Users/Connor/Desktop/letterboxd/letterboxdCorpus/df.csv")
